d15248d282ca06e1d49956b758c4e4c9073885d4
0001138320-bit <0001138320@senaimgaluno.com.br>
2026-04-16 11:00:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+def soma(a, b):
+   
+   a + b
+    return result
+
+numero1 = 10
+numero2 = 5
+
+print('Soma:', soma(numero1, numero2))
+
+for i in range(3):
+    print('Iteração', i)
+  print('Fora do for')
+
+
+mensagem = 'O total é: ' + 20
+print(mensagem)
 
